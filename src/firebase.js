725d66d82a25c001e11656ca8fcd91fcd55f6d69
Harjot Singh <harjotsingh0723@gmail.com>
2020-08-27 15:40:39 -0700

    Test

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDea1Q0XzG53pljOjJt9_Odpump4W-86f0",
+    authDomain: "netflix-clone-ee717.firebaseapp.com",
+    databaseURL: "https://netflix-clone-ee717.firebaseio.com",
+    projectId: "netflix-clone-ee717",
+    storageBucket: "netflix-clone-ee717.appspot.com",
+    messagingSenderId: "1009578485900",
+    appId: "1:1009578485900:web:523eb4cd3779216f35f3a9",
+    measurementId: "G-VVP3Z1T48E"
+  };
+
+  //https://netflix-clone-ee717.web.app/
